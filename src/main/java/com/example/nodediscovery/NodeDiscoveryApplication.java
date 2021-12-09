62aa164f140fc6f342f22ee50ca3d0714a02af52
muskaplan <muskaplan@innova.com.tr>
2021-12-09 16:10:58 +0300

    Initial commit

---
+++ src/main/java/com/example/nodediscovery/NodeDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.nodediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodeDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodeDiscoveryApplication.class, args);
+	}
+
+}
